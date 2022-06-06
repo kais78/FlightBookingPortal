2bcd02066f9f5ada4242686d68f706ded47839a4
kais78 <kais2020@yahoo.com>
2022-06-06 17:09:34 +0200

    flight class implimented

--- src/Flight.java
+++ src/Flight.java
@@ -1,3 +1,65 @@
 public class Flight {
+    String date;
+    String deptCity;
+    String destCity;
+    String economyClass;
+    double economyClassPrice;
+    String businessClass;
+    double businessClassPrice;
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDeptCity() {
+        return deptCity;
+    }
+
+    public void setDeptCity(String deptCity) {
+        this.deptCity = deptCity;
+    }
+
+    public String getDestCity() {
+        return destCity;
+    }
+
+    public void setDestCity(String destCity) {
+        this.destCity = destCity;
+    }
+
+    public String getEconomyClass() {
+        return economyClass;
+    }
+
+    public void setEconomyClass(String economyClass) {
+        this.economyClass = economyClass;
+    }
+
+    public double getEconomyClassPrice() {
+        return economyClassPrice;
+    }
+
+    public void setEconomyClassPrice(double economyClassPrice) {
+        this.economyClassPrice = economyClassPrice;
+    }
+
+    public String getBusinessClass() {
+        return businessClass;
+    }
+
+    public void setBusinessClass(String businessClass) {
+        this.businessClass = businessClass;
+    }
+
+    public double getBusinessClassPrice() {
+        return businessClassPrice;
+    }
+
+    public void setBusinessClassPrice(double businessClassPrice) {
+        this.businessClassPrice = businessClassPrice;
+    }
 }
--- src/Operations.java
+++ src/Operations.java
@@ -8,16 +8,21 @@ public class Operations implements Functions{
         int inp;
         while(true){
         System.out.println("!!!!Welcome to the flight booking system!!!!");
-        System.out.println("please select from the following menu.....");
+        System.out.println("\nplease select from the following menu.....");
         System.out.println("\n 1. Press 1 if you are a customer." +
-                "2. Press 2 if you are a Admin Person" +
-                "3. Press 3 to exit");
+                "\n2. Press 2 if you are a Admin Person" +
+                "\n3. Press 3 to exit");
         inp = in.nextInt();
         if (inp == 1){
+            //calling customer menu function
+            CustomerMenu();
 
         }else if(inp == 2){
+            //Calling ADMIN MENU function
+            AdminMenu();
 
         }else if( inp == 3){
+            //Closing the program
             System.out.println("!!!!Welcome Back Again!!!!");
             System.exit(0);
         }
@@ -29,11 +34,45 @@ public class Operations implements Functions{
     }
     }
 
-    private void CusstomerMenu(){
+    private void CustomerMenu(){
+        System.out.println("****Welcome Valued Customer****");
+        System.out.println("Please Select from the following menu");
+        System.out.println("1. Press 1 to search a flight.");
+        System.out.println("2. Press 2 to book a flight.");
+        System.out.println("3. Press 3 to check your flight booking status.");
+        System.out.println("4. Press 4 to cancel a flight.");
+        System.out.println("5. Press 5 to edit your flight.");
+        System.out.println("6. Press 6 to exit.");
+
+    }
+
+
+    private void searchFlight(){
+
+    }
+
+    private void bookFlight(){
 
     }
 
     private void AdminMenu(){
+        System.out.println("****Welcome to the Admin Menu****");
+        System.out.println("1. Press 1 to check the available flight" +
+                "\n2. Press 2 to check the register customers" +
+                "\n3. Press 3 to add new flight" +
+                "\n4. Press 4 to delete a flight" +
+                "\n5. Press 5 delete all flight" +
+                "\n6. Press 6 to delete a register customer" +
+                "\n7. Press 7 to delete all register customer" +
+                "\n8. Press 8 to exit");
+
 
     }
+
+
+    private void addFlight(){
+
+    }
+
+
 }

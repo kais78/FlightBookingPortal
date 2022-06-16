f18fff2a4d04c5fd4d861865344c79023ac07fa0
kais78 <kais2020@yahoo.com>
2022-06-16 09:41:26 +0200

    test save

---
+++ src/FileUtility.java
@@ -0,0 +1,26 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.*;
+
+public class FileDetails {
+
+
+    public static void saveObject(String filename, Object o, StandardOpenOption... option) {
+        Path path = Paths.get(filename);
+        try (ObjectOutputStream out = new ObjectOutputStream(Files.newOutputStream(path, option))) {
+            out.writeObject(o);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Object loadObject(String filename) {
+        Path path = Paths.get(filename);
+        try (ObjectInputStream in = new ObjectInputStream(Files.newInputStream(path))) {
+            return in.readObject();
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+}
